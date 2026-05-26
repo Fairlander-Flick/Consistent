9fad7d796b2368d1eb9f05ca9ee77338c9884b12
Ugur Tasdan <ugurtasdanflick@protonmail.com>
2026-05-26 22:43:38 +0200

    feat(login): add login.css with split layout and responsive styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/tokens.css'
+import './styles/login.css'
 import App from './App.jsx'
 import { registerSW } from './lib/registerSW'
 
